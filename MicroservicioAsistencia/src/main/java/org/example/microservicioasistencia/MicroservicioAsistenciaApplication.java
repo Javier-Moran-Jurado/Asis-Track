aa0fc1e96f471aa4d86dcdcfb05956744e1b4da0
Javier-Moran-Jurado <javier.moran02@uceva.edu.co>
2026-03-16 11:35:12 -0500

    creacion de rama y proyecto asistencia

---
+++ MicroservicioAsistencia/src/main/java/org/example/microservicioasistencia/MicroservicioAsistenciaApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservicioasistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioAsistenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioAsistenciaApplication.class, args);
+    }
+
+}
