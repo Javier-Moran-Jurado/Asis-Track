74b3903769823c75515613a2041713e0697a8d86
Javier-Moran-Jurado <javier.moran02@uceva.edu.co>
2026-03-19 10:48:06 -0500

    Agregando MicroservicioJustificacion

---
+++ Documentos/Proyecto-7semestre/MicroServicioJustificacion/src/main/java/org/example/microserviciojustificacion/MicroServicioJustificacionApplication.java
@@ -0,0 +1,13 @@
+package org.example.microserviciojustificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioJustificacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicioJustificacionApplication.class, args);
+    }
+
+}
